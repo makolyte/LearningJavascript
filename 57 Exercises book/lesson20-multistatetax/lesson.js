234afd779c09fad74f321b56ce91d9cf43749c34
Maclain Wiltzer <mwiltzer@gmail.com>
2016-02-15 08:18:15 -0400

    Lesson 20 - 1. append() is jquery. appendChild() is js 2. put js includes on the bottom of body tag. I was including it in the header and it wasn't building the page with js, which is because the html wasn't loaded when the js was executing. Moving it to the bottom reversed that.

---
+++ 57 Exercises book/lesson20-multistatetax/lesson.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Mac on 1/30/2016.
+ */
+
+buildPage = function(){
+    input = $("#divInput")
+
+    input.append("State <input type='text' id='state'><br>");
+    input.append("County <input type='text' id='county'><br>");
+    input.append("Order Amount <input type='number' id='amount'><br>");
+    input.append("<button onclick='doStuff()'>Calc tax</button>");
+
+}
+
+buildPage()
+
+//Learned i needed to put the <script at the bottom of the HTML, because
+//it was calling this at the beginning, when
+//there wasn't any elements on the page
+
+doStuff = function(){
+    orderAmt = parseFloat( $("#amount").val())
+    state = $("#state").val()
+    county = $("#county").val()
+
+    tax = 0
+
+    if (state == "WI")
+    {
+        if (county == "1")
+            tax = orderAmt * 0.05
+        else if (county == "2")
+            tax = orderAmt * 0.10
+        else
+            tax = orderAmt * 0.025
+    }
+
+    output = "Subtotal " + orderAmt + "<br>"
+        + " Tax " + tax + "<br>"
+        + "Total " + (tax + orderAmt)
+
+    $("#output").append(output)
+}
+
+
