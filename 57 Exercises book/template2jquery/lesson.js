f590ab879f461be876251a5e25b4469c719fd733
Maclain Wiltzer <mwiltzer@gmail.com>
2016-02-15 08:12:34 -0400

    Changed this so i build the page in javascript instead of in the html

--- 57 Exercises book/template2jquery/lesson.js
+++ 57 Exercises book/template2jquery/lesson.js
@@ -2,13 +2,42 @@
  * Created by Mac on 1/30/2016.
  */
 
+buildPage = function(){
+    input = $("#divInput")
+
+    input.append("State <input type='text' id='state'><br>");
+    input.append("County <input type='text' id='county'><br>");
+    input.append("Order Amount <input type='number' id='amount'><br>");
+    input.append("<button onclick='doStuff()'>Calc tax</button>");
+
+}
+
+buildPage()
+
+//Learned i needed to put the <script at the bottom of the HTML, because
+//it was calling this at the beginning, when
+//there wasn't any elements on the page
+
 doStuff = function(){
-    var output = ["Do you", "{verb}", "your", "{adjective}",  "{noun}", "{adverb}", "? That's hilarious!"]
-    output[1] = document.getElementById("txtVerb").value
-    output[3] = document.getElementById("txtAdj").value
-    output[4] = document.getElementById("txtNoun").value
-    output[5] = document.getElementById("txtAdverb").value
-    document.getElementById("txtOutput").innerText = output.join(" ")
+    orderAmt = parseFloat( $("#amount").val())
+    state = $("#state").val()
+    county = $("#county").val()
+
+    tax = 0
+
+    if (state == "WI")
+    {
+        if (county == "1")
+            tax = orderAmt * 0.05
+        else if (county == "2")
+            tax = orderAmt * 0.10
+        else
+            tax = orderAmt * 0.025
+    }
+
+    output =
+
+    $("#output")
 }
 
 
