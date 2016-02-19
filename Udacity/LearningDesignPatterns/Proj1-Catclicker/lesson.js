4afcf34840548d0c60e76613a51b7a31e2668b5e
Maclain Wiltzer <mwiltzer@gmail.com>
2016-02-19 08:46:31 -0400

    Added two cats and cat names

--- Udacity/LearningDesignPatterns/Proj1-Catclicker/lesson.js
+++ Udacity/LearningDesignPatterns/Proj1-Catclicker/lesson.js
@@ -2,16 +2,29 @@
  * Created by Mac on 1/30/2016.
  */
 
-buildPage = function(){
+catAdder = function(name, imgURL)
+{
     input = $("#divInput")
+    var outputID = name +"clicks"
+    var imgID = name + "img"
+    input.append("<div id='" + name + "'>")
+    input.append("<h1>"+name+"</h1><br>")
+    input.append("<img src='" + imgURL+ "' alt='" + name +"' id='" + imgID + "'><br>")
+    input.append("Clicks: <div id='" + outputID + "'>0</div>")
+    input.append("</div>")
 
-    input.append("<img src='https://c2.staticflickr.com/2/1126/625069434_db86b67df8_n.jpg' alt='cat' id='imgcat'><br>")
-    $("#output").text("0")
 
-    $("#imgcat").click(function(){
-        var curClicks = parseInt($("#output").text())
-        $("#output").text(curClicks+1)
+    $("#" + imgID).click(function(){
+        var curClicks = parseInt($("#" + outputID).text())
+        $("#" + outputID).text(curClicks+1)
     })
+}
+
+buildPage = function(){
+
+
+    catAdder("tabby", "https://c2.staticflickr.com/2/1126/625069434_db86b67df8_n.jpg" )
+    catAdder("chewie", "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496")
 }
 
 $(buildPage)
