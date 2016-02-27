1dc65b3f5fd8de042efde249e25ae1620b2c1534
Maclain Wiltzer <mwiltzer@gmail.com>
2016-02-27 19:20:38 -0400

    Done with course

--- Udacity/LearningOOJS/Frogger/js/app.js
+++ Udacity/LearningOOJS/Frogger/js/app.js
@@ -1,35 +1,50 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Entity = function(x, y, speed, image)
+{
+    this.sprite = image;
+    this.y = y
+    this.x = x
+    this.speed = speed
+}
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+Entity.prototype.update = function(dt) {
+    this.x += this.speed * dt;
 };
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Entity.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
+// Pseudoclassical pattern
+var Enemy = function(x, y, speed) {
+    Entity.call(this, x, y, speed, 'images/enemy-bug.png')
+};
+Enemy.prototype = Object.create(Entity.prototype)
+Enemy.prototype.constructor = Enemy
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var enemy1 = new Enemy(-50, 50, 50*2)
+var enemy2 = new Enemy(-100, 50+85, 25)
+var enemy3 = new Enemy(-100, 50+85+85, 50*2)
+var allEnemies = [enemy1, enemy2, enemy3]
 // Place the player object in a variable called player
 
+var Player = function(x, y, speed){
+    Entity.call(this, x, y, speed, 'images/char-boy.png')
+}
+Player.prototype = Object.create(Entity.prototype)
+Player.prototype.constructor = Player
+Player.prototype.update = function(dt){
+    this.y -= this.speed ;
+}//override, do nothing
+
+Player.prototype.handleInput = function(key)
+{
+    //do nothing, make the player AI only
+}
+var player = new Player(300, 400, 1)
 
 
 // This listens for key presses and sends the keys to your
